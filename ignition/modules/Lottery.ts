7ac7cdeb90a17d4c38a9d70d3078af3c1b0f86d8
DallaValleChristian <6190044@itisrossi.vi.it>
2025-05-02 10:10:35 +0200

    Deleted old file

--- ignition/modules/Lottery.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const LotteryModule = buildModule("LotteryModule", (m) => {
-  const lottery = m.contract("Lottery");
-
-  return { lottery };
-});
-
-export default LotteryModule;
